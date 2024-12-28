4996e6385684a10eacc858d5b9a800cc037e3a4d
zarapatel117 <zarapatel2510@gmail.com~>
2024-12-28 15:53:43 +0530

    Right angle triangle

---
+++ Right angle triangle.py
@@ -0,0 +1,7 @@
+print("this is a Right angle triangle")
+n = int(input("enter the number of rows you want: "))
+
+for i in range(n):
+    for j in range (i+1):
+         print("*",end="")
+    print()        
